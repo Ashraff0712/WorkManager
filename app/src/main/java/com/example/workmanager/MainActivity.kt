9e7a5935dab927f3689684e23af842ffcc513985
Ashraff0712 <Ashraff*7416>
2022-09-24 11:31:50 +0530

    1 Created a one time request with initial delay

--- app/src/main/java/com/example/workmanager/MainActivity.kt
+++ app/src/main/java/com/example/workmanager/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+        //One Time Work Request with initial delay
         val yourWorkRequest = OneTimeWorkRequest.Builder(MainActivityWorkerClass::class.java).
                 setInitialDelay(10,TimeUnit.SECONDS).build()
                 WorkManager.getInstance(this).enqueue(yourWorkRequest)
